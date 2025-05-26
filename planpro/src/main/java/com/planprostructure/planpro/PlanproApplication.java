4ee973786cc4543ed3adcf6e5af50ba7c5171785
Ty Hen <hentyna11@gmail.com>
2025-05-26 13:38:25 +0700

    v1

---
+++ planpro/src/main/java/com/planprostructure/planpro/PlanproApplication.java
@@ -0,0 +1,13 @@
+package com.planprostructure.planpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanproApplication.class, args);
+    }
+
+}
